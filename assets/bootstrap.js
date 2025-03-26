832cbc64db8be0ca9d0d428dc8ac6d8ec24f554d
John Doe <mariejosee89@hotmail.fr>
2025-03-26 09:56:31 +0100

    Style: Installation et mise en fonction de Bootstrap.

--- assets/bootstrap.js
+++ assets/bootstrap.js
@@ -3,3 +3,8 @@ import { startStimulusApp } from '@symfony/stimulus-bundle';
 const app = startStimulusApp();
 // register any custom, 3rd party controllers here
 // app.register('some_controller_name', SomeImportedController);
+
+// ajout de Boostrap dans nos scripts
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '@popperjs/core';
+import 'bootstrap';
