4923344a6112bf5d2f4228fa8a3ab0596740670d
John Doe <mariejosee89@hotmail.fr>
2025-03-26 10:07:40 +0100

    Ci: Mise en place de la route de la page d'accueil + modification de son contenu.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -8,7 +8,8 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    //modification de la route
+    #[Route('/', name: 'app_home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
