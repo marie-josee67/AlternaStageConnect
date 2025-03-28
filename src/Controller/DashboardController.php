ac65c04a8abcfef5c2becad5eb94aa20ebfe5fd3
John Doe <mariejosee89@hotmail.fr>
2025-03-28 15:29:54 +0100

    Perf: optimisation du code.

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,15 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        // URL de la forme : localhost:8000/dashboard?lastpictures=3
+        // dd($request); // Debugband Die d'une variable, affiche les détails et coupe l'exécution du code => var_dump(...) die;
+        $intLastPictures = $request->query->get('lastpictures',10);// valeur par défault 10
+        dd($intLastPictures);
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
         ]);
