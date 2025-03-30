b6fb05077b08db2688593886a4574d248f4ca0f6
John Doe <mariejosee89@hotmail.fr>
2025-03-30 17:23:23 +0200

    Création d'annonce.

--- src/Controller/AnnonceController.php
+++ src/Controller/AnnonceController.php
@@ -2,25 +2,46 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use DateTime;
+use App\Entity\Annonce;
+use APP\Repository\AnnonceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class AnnonceController extends AbstractController
 {
     #[Route('/annonce', name: 'app_annonce')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $annonceRepository = $entityManager->getRepository(Annonce::class);
+        $annonces = $annonceRepository->findAll();
+
         return $this->render('annonce/index.html.twig', [
-            'controller_name' => 'AnnonceController',
+            'annonces' => $annonces,
         ]);
     }
 
     #[Route('/annonce/create', name: 'app_annonce_create')]
-    public function create(): Response
+    public function create(EntityManagerInterface $entityManager): Response
     {
+        // création de l'objet
+        $annonce = new Annonce();
+
+        // définir les différent attributs de l'objet
+        $annonce->setDescription("annonce 1")
+                ->setDate(new DateTime())
+                ->setFilname("fichier.img");
+
+        // prépare les données à être sauvegarder en base
+        $entityManager->persist($annonce);
+
+        //enregistre les données en base et créer l'Id unique
+        $entityManager->flush();
+
         return $this->render('annonce/create.html.twig', [
-            'controller_name' => 'AnnonceController',
+            'annonce'=>$annonce
         ]);
     }
 }
