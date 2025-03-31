a0d8b89d071f0336091f15839bf20808124ff59c
John Doe <mariejosee89@hotmail.fr>
2025-03-31 20:03:54 +0200

    Rajout de LoggerInterface sur le controller Dashboard.

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,11 +11,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 final class DashboardController extends AbstractController
 {
     #[Route('/dashboard', name: 'app_dashboard')]
-    public function index(Request $request): Response
+    public function index(Request $request, LoggerInterface $logger): Response
     {
         // URL de la forme : localhost:8000/dashboard?lastpictures=3
         // dd($request); // Debugband Die d'une variable, affiche les détails et coupe l'exécution du code => var_dump(...) die;
         $intLastPictures = $request->query->get('lastpictures',10);// valeur par défault 10
+        $logger->info('Last Picture from URL: '. $intLastPictures);
         // dd($intLastPictures);
         return $this->render('dashboard/index.html.twig', [
             'controller_name' => 'DashboardController',
