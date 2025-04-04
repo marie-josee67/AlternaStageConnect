1ca51434810ab7daa5845a66226ace71579d0309
John Doe <mariejosee89@hotmail.fr>
2025-04-04 13:38:14 +0200

    Style: mise en forme du formulaire

--- src/Form/AnnonceType.php
+++ src/Form/AnnonceType.php
@@ -6,17 +6,29 @@ use App\Entity\Annonce;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class AnnonceType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('filname')
+            ->add('filname', TextType::class,[
+                'label' => "Nom de l'annonce"
+            ])
             ->add('date', null, [
                 'widget' => 'single_text',
+                'label' => "Date de l'annonce"
             ])
-            ->add('description')
+            ->add('description', null, [
+                'label'=>"Description de l'offre"
+            ])
+            
+            // ajout du bouton
+            ->add('submit', SubmitType::class, [
+                'label'=> 'Enregistrer'
+            ])
         ;
     }
 
