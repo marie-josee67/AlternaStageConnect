9b98eba149ba1a5b3c429417e9726268db0d6259
John Doe <mariejosee89@hotmail.fr>
2025-04-06 16:16:19 +0200

    Feat: mise en place de la liste des métier dans le Builder pour la création des annonces.

--- src/Form/AlternanceType.php
+++ src/Form/AlternanceType.php
@@ -6,6 +6,8 @@ use App\Entity\Alternance;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class AlternanceType extends AbstractType
@@ -16,9 +18,178 @@ class AlternanceType extends AbstractType
             ->add('titre', null, [
                 'label'=>"Titre de l'annonce *"
             ])
-            ->add('metier', null, [
-                'label'=>"Métier *"
+            ->add('metier', ChoiceType::class, [
+                'label' => 'Métier',
+                'choices' => [
+                    
+                    'Informatique et Numérique' => [
+                        'Développeur' => 'Developpeur',
+                        'Développeur Full Stack' => 'Developpeur Full Stack',
+                        'Développeur Front-End' => 'Developpeur Front-End',
+                        'Développeur Back-End' => 'Developpeur Back-End',
+                        'Administrateur Systèmes et Réseaux' => 'Administrateur Systèmes et Réseaux',
+                        'Ingénieur DevOps' => 'Ingénieur DevOps',
+                        'Data Scientist' => 'Data Scientist',
+                        'Analyste Cybersécurité' => 'Analyste Cybersécurité',
+                        'UX/UI Designer' => 'UX/UI Designer',
+                        'Chef de Projet IT' => 'Chef de Projet IT',
+                        'Administrateur de Base de Données' => 'Administrateur de Base de Données',
+                        'Développeur Mobile' => 'Développeur Mobile',
+                        'Ingénieur Logiciel' => 'Ingénieur Logiciel',
+                        'Testeur QA' => 'Testeur QA',
+                        'Expert en Blockchain' => 'Expert en Blockchain',
+                        'Consultant en Cybersécurité' => 'Consultant en Cybersécurité',
+                    ],
+
+                    'BTP et Construction' => [
+                        'Architecte' => 'Architecte',
+                        'Maçon' => 'Maçon',
+                        'Électricien' => 'Électricien',
+                        'Plombier' => 'Plombier',
+                        'Ingénieur en BTP' => 'Ingénieur en BTP',
+                        'Conducteur de Travaux' => 'Conducteur de Travaux',
+                        'Charpentier' => 'Charpentier',
+                        'Urbaniste' => 'Urbaniste',
+                        'Ingénieur Géotechnique' => 'Ingénieur Géotechnique',
+                        'Conducteur de Travaux Publics' => 'Conducteur de Travaux Publics',
+                        'Chef de Chantier' => 'Chef de Chantier',
+                        'Ingénieur Électrique' => 'Ingénieur Électrique',
+                        'Technicien en Maintenance' => 'Technicien en Maintenance',
+                    ],
+
+                    'Santé et Paramédical' => [
+                        'Médecin Généraliste' => 'Médecin Généraliste',
+                        'Infirmier' => 'Infirmier',
+                        'Chirurgien' => 'Chirurgien',
+                        'Pharmacien' => 'Pharmacien',
+                        'Sage-Femme' => 'Sage-Femme',
+                        'Kinésithérapeute' => 'Kinésithérapeute',
+                        'Psychologue' => 'Psychologue',
+                        'Médecin Spécialiste' => 'Médecin Spécialiste',
+                        'Chirurgien-dentiste' => 'Chirurgien-dentiste',
+                        'Orthophoniste' => 'Orthophoniste',
+                        'Opticien-Lunetier' => 'Opticien-Lunetier',
+                        'Dentiste' => 'Dentiste',
+                    ],
+
+                    'Commerce et Vente' => [
+                        'Commercial' => 'Commercial',
+                        'Représentant des Ventes' => 'Représentant des Ventes',
+                        'Responsable de Magasin' => 'Responsable de Magasin',
+                        'Chargé de Clientèle' => 'Chargé de Clientèle',
+                        'Directeur Commercial' => 'Directeur Commercial',
+                        'Agent Immobilier' => 'Agent Immobilier',
+                        'Assistant Commercial' => 'Assistant Commercial',
+                        'Responsable Marketing' => 'Responsable Marketing',
+                        'Chef de Rayon' => 'Chef de Rayon',
+                        'Vendeur en ligne' => 'Vendeur en ligne',
+                        'Négociant' => 'Négociant',
+                    ],
+
+                    'Industrie et Production' => [
+                        'Technicien de Production' => 'Technicien de Production',
+                        'Opérateur de Machine' => 'Opérateur de Machine',
+                        'Soudeur' => 'Soudeur',
+                        'Ingénieur en Production' => 'Ingénieur en Production',
+                        'Mécanicien Industriel' => 'Mécanicien Industriel',
+                        'Ingénieur Mécanique' => 'Ingénieur Mécanique',
+                        'Technicien en Maintenance Industrielle' => 'Technicien en Maintenance Industrielle',
+                        'Ingénieur Environnement' => 'Ingénieur Environnement',
+                        'Conducteur de ligne de production' => 'Conducteur de ligne de production',
+                        'Chargé d\'études industrielles' => 'Chargé d\'études industrielles',
+                    ],
+
+                    'Transport et Logistique' => [
+                        'Chauffeur Routier' => 'Chauffeur Routier',
+                        'Livreur' => 'Livreur',
+                        'Magasinier' => 'Magasinier',
+                        'Responsable Logistique' => 'Responsable Logistique',
+                        'Pilote de Ligne' => 'Pilote de Ligne',
+                        'Logisticien' => 'Logisticien',
+                        'Chef de Parc' => 'Chef de Parc',
+                        'Responsable Transport' => 'Responsable Transport',
+                        'Technicien en Logistique' => 'Technicien en Logistique',
+                        'Pilote de Drone' => 'Pilote de Drone',
+                    ],
+
+                    'Hôtellerie et Restauration' => [
+                        'Chef Cuisinier' => 'Chef Cuisinier',
+                        'Serveur' => 'Serveur',
+                        'Réceptionniste' => 'Réceptionniste',
+                        'Pâtissier' => 'Pâtissier',
+                        'Sommelier' => 'Sommelier',
+                        'Barman' => 'Barman',
+                        'Responsable des Ressources Humaines (Hôtellerie)' => 'Responsable des Ressources Humaines (Hôtellerie)',
+                        'Directeur d’Hôtel' => 'Directeur d’Hôtel',
+                    ],
+
+                    'Communication et Marketing' => [
+                        'Chargé de Communication' => 'Chargé de Communication',
+                        'Community Manager' => 'Community Manager',
+                        'Chef de Produit' => 'Chef de Produit',
+                        'SEO Manager' => 'SEO Manager',
+                        'Graphiste' => 'Graphiste',
+                        'Publicitaire' => 'Publicitaire',
+                        'Spécialiste en Publicité' => 'Spécialiste en Publicité',
+                        'Directeur Marketing' => 'Directeur Marketing',
+                        'Photographe' => 'Photographe',
+                        'Rédacteur Web' => 'Rédacteur Web',
+                        'Influenceur' => 'Influenceur',
+                        'Chef de Produit Digital' => 'Chef de Produit Digital',
+                    ],
+
+                    'Éducation et Formation' => [
+                        'Professeur des Écoles' => 'Professeur des Écoles',
+                        'Formateur' => 'Formateur',
+                        'Éducateur Spécialisé' => 'Éducateur Spécialisé',
+                        'Moniteur' => 'Moniteur',
+                        'Animateur Périscolaire' => 'Animateur Périscolaire',
+                        'Coach en Développement Personnel' => 'Coach en Développement Personnel',
+                        'Formateur en Entreprise' => 'Formateur en Entreprise',
+                        'Psychopédagogue' => 'Psychopédagogue',
+                    ],
+
+                    'Juridique et Administratif' => [
+                        'Avocat' => 'Avocat',
+                        'Notaire' => 'Notaire',
+                        'Secrétaire Administratif' => 'Secrétaire Administratif',
+                        'Juriste' => 'Juriste',
+                        'Huissier de Justice' => 'Huissier de Justice',
+                        'Greffier' => 'Greffier',
+                        'Conseiller en Propriété Industrielle' => 'Conseiller en Propriété Industrielle',
+                        'Avocat en Droit des Affaires' => 'Avocat en Droit des Affaires',
+                        'Notaire Assistant' => 'Notaire Assistant',
+                        'Administrateur Judiciaire' => 'Administrateur Judiciaire',
+                    ],
+
+                    'Agriculture et Environnement' => [
+                        'Agriculteur' => 'Agriculteur',
+                        'Vigneron' => 'Vigneron',
+                        'Ingénieur Agronome' => 'Ingénieur Agronome',
+                        'Technicien de l\'Environnement' => 'Technicien de l\'Environnement',
+                        'Écologiste' => 'Écologiste',
+                        'Technicien en Agriculture Biologique' => 'Technicien en Agriculture Biologique',
+                        'Garde Forestier' => 'Garde Forestier',
+                        'Spécialiste en Gestion des Ressources Naturelles' => 'Spécialiste en Gestion des Ressources Naturelles',
+                    ],
+
+                    'Sport et Animation' => [
+                        'Coach Sportif' => 'Coach Sportif',
+                        'Animateur' => 'Animateur',
+                        'Éducateur Sportif' => 'Éducateur Sportif',
+                        'Préparateur Physique' => 'Préparateur Physique',
+                        'Entraîneur Sportif' => 'Entraîneur Sportif',
+                        'Moniteur de Ski' => 'Moniteur de Ski',
+                        'Kinésithérapeute Sportif' => 'Kinésithérapeute Sportif',
+                    ],
+
+                    'Autre' => [
+                        'Autre' => 'autre'
+                    ]
+                ],
+                'placeholder' => 'Choisissez un métier',
             ])
+            
             ->add('img', null, [
                 'label'=>"Image *"
             ])
