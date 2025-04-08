a4c2ef8c8591daf4a90c40e64146392d6aacbcb6
John Doe <mariejosee89@hotmail.fr>
2025-04-08 16:03:01 +0200

    Mise en place d'annonces aléatoire des alternance et des stage sur la page d'accueil.

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,18 +2,32 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Repository\StageRepository;
+use App\Repository\AlternanceRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 final class HomeController extends AbstractController
 {
-    //modification de la route
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(AlternanceRepository $alternanceRepository, StageRepository $stageRepository): Response
     {
+        //récupération de toute les annonces des alternances et des stage
+        $alternances = $alternanceRepository->findAll();
+        $stages = $stageRepository->findAll();
+
+        // mélanger pour avoir des cards d'annonce aléatoire
+        shuffle($alternances);
+        shuffle($stages);
+
+        // 8 éléments aléatoires par catégorie
+        $selectedAlternances = array_slice($alternances, 0, 8);
+        $selectedStages = array_slice($stages, 0, 8);
+
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'alternances' => $selectedAlternances,
+            'stages' => $selectedStages,
         ]);
     }
 }
