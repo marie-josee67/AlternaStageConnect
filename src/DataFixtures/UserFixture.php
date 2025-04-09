53baf989036aa4265b8df6c02a98509820ca2a3d
John Doe <mariejosee89@hotmail.fr>
2025-04-09 20:10:01 +0200

    Chore: création d'utilisateur 2 choisi + 5 aléatoires.

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -6,20 +6,50 @@ use Faker\Factory;
 use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserFixture extends Fixture
 {
-    public function load(ObjectManager $manager): void
+
+    public function __construct(
+        private UserPasswordHasherInterface $userPasswordHasher
+    )
     {
+        
+    }
+
+    public function load(ObjectManager $manager, ): void
+    {
+        $nbUsers = 5;
         $faker = Factory::create('fr_FR');
+
+        // utilisateur 1 
         $user = new User();
+        $user->setEmail("Prubis@app.fr")
+            ->setFirstname("Plume")
+            ->setLastname('Rubis');
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, "qsdfghjklm123456/"));
+        $manager->persist($user);
+
+        // utilisateur 2 
+        $user = new User();
+        $user->setEmail("developpeuse@outlook.fr")
+            ->setFirstname("Marie-josée")
+            ->setLastname('Schmitt');
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, "azertyuiop123456/"));
+        $manager->persist($user);
+
+        // utilisateur de masse 
+        for($i = 0; $i < $nbUsers; $i++){
+            $user = new User();
         $user->setEmail($faker->email())
             ->setFirstname($faker->firstName())
             ->setLastname($faker->lastName());
 
-        $user->setPassword($faker->password());
-
+        $user->setPassword($this->userPasswordHasher->hashPassword($user, $faker->password()));
         $manager->persist($user);
+        }
+      
         $manager->flush();
     }
 }
