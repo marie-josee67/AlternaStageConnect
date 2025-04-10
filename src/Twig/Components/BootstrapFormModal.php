809ddc4bd371da5c95e912883013e951bf1b3fe6
John Doe <mariejosee89@hotmail.fr>
2025-04-10 19:49:21 +0200

    Style: mise en forme de la moda + contenu .

--- src/Twig/Components/BootstrapFormModal.php
+++ src/Twig/Components/BootstrapFormModal.php
@@ -7,4 +7,9 @@ use Symfony\UX\TwigComponent\Attribute\AsTwigComponent;
 #[AsTwigComponent]
 final class BootstrapFormModal
 {
+    public string $btnText;
+    public string $btnType;
+    public string $modalId;
+    public string $modalTitle;
+
 }
