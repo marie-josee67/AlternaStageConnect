6c880b6dadf240091191495dbc5c80707bcef3c8
John Doe <mariejosee89@hotmail.fr>
2025-04-14 16:56:25 +0200

    User role (modal + twig extensiion) là suite.

--- src/Twig/Runtime/HasRoleExtensionRuntime.php
+++ src/Twig/Runtime/HasRoleExtensionRuntime.php
@@ -6,8 +6,11 @@ use Symfony\Component\Security\Core\Role\RoleHierarchyInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Twig\Extension\RuntimeExtensionInterface;
 
+// ********************************************** Interface de gestion des rôles utilisateurs **************************************** */
+
 class HasRoleExtensionRuntime implements RuntimeExtensionInterface
 {
+    // permet de connaitre le rôle de l'utilisateur et la hierarchie des rôles
     public function __construct(
         private RoleHierarchyInterface $roleHierarchy
     )
@@ -18,6 +21,8 @@ class HasRoleExtensionRuntime implements RuntimeExtensionInterface
     public function hasRole(UserInterface $user, string $role): bool
     {
         return in_array($role,
+        
+        //verification que le rôle est dans le tableau des rôles
         $this->roleHierarchy->getReachableRoleNames($user->getRoles()));
     }
 }
