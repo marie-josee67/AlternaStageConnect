8ca2c83051c453a5b9bc6ee19a82858a024b14f0
John Doe <mariejosee89@hotmail.fr>
2025-04-14 17:43:26 +0200

    Traduction du texte.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -10,7 +10,8 @@ use Symfony\Component\Security\Core\User\UserInterface;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
-#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
+#[UniqueEntity(fields: ['email'], message: 'Il y a déjà un compte avec cet e-mail')]
+
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
