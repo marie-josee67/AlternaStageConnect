e23a519a0855efa087c4404ce517c635b5e7a28f
John Doe <mariejosee89@hotmail.fr>
2025-04-15 12:33:21 +0200

    suppression de toutes les migration puis lancement d'un enouvelle.

--- migrations/Version20250415103246.php
+++ migrations/Version20250415103246.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250415095356 extends AbstractMigration
+final class Version20250415103246 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20250415095356 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE alternance ADD CONSTRAINT FK_445F37B9B03A8386 FOREIGN KEY (created_by_id) REFERENCES user (id)');
+        $this->addSql('ALTER TABLE alternance CHANGE created_by_id created_by_id INT DEFAULT NULL');
+        $this->addSql('ALTER TABLE alternance ADD CONSTRAINT FK_445F37B9A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE alternance DROP FOREIGN KEY FK_445F37B9B03A8386');
+        $this->addSql('ALTER TABLE alternance DROP FOREIGN KEY FK_445F37B9A76ED395');
+        $this->addSql('ALTER TABLE alternance CHANGE created_by_id created_by_id INT NOT NULL');
     }
 }
